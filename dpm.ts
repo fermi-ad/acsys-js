f7ffa835a3e5f2c7dead534cf288f7c4d870203c
Rich Neswold <neswold@fnal.gov>
2019-04-22 15:40:31 -0500

    Many changes.
    Highlights include:
    
    * Defined Typescript interfaces for the callback routines. These interfaces are mostly compatible with the DPM reply messages, but now we don't have to re-export the protocol compiler messages.
    * The main, background process now handles unhandled exceptions by reporting them to the log, canceling the ACNET requests, and retrying the connection. The previous behavior was to terminate the background process.

--- dpm.ts
+++ dpm.ts
@@ -8,7 +8,6 @@ import {
   DPM_request_StopList,
   DPM_reply_StartList,
   DPM_request_AddToList,
-  DPM_reply_ServiceDiscovery,
   DPM_reply_OpenList,
   DPM_reply_ListStatus,
   DPM_reply_Status,
@@ -22,13 +21,71 @@ import {
   DPM_reply_DigitalAlarm,
   DPM_reply_BasicStatus,
   DPM_Replies
-} from "./dist/dpm_protocol.js";
+} from "./dist/dpm_protocol";
 
-interface Requests {
-  request: string;
-  callback: (d: any, i: DPM_reply_DeviceInfo) => void;
-  errCallback?: (v: DPM_reply_Status) => void;
-  dInfo?: DPM_reply_DeviceInfo;
+interface AnaAlarm {
+  minimum: number;
+  maximum: number;
+  alarm_enable: boolean;
+  alarm_status: boolean;
+  abort: boolean;
+  abort_inhibit: boolean;
+  tries_needed: number;
+  tries_now: number;
+}
+
+interface DigAlarm {
+  nominal: number;
+  mask: number;
+  alarm_enable: boolean;
+  alarm_status: boolean;
+  abort: boolean;
+  abort_inhibit: boolean;
+  tries_needed: number;
+  tries_now: number;
+}
+
+interface BasStatus {
+  on?: boolean;
+  ready?: boolean;
+  remote?: boolean;
+  positive?: boolean;
+  ramp?: boolean;
+}
+
+interface DataReply {
+  ref_id: number;
+  timestamp: number;
+  data:
+    | number
+    | string
+    | AnaAlarm
+    | DigAlarm
+    | BasStatus
+    | Array<number>
+    | Array<string>
+    | ArrayBuffer;
+}
+
+interface DeviceInfo {
+  di: number;
+  name: string;
+  description: string;
+  units?: string;
+  format_hint?: number;
+}
+
+interface DataStatus {
+  ref_id: number;
+  timestamp: number;
+  status: Status;
+}
+
+interface Request {
+  drf2: string;
+  callback: (d: DataReply, i: DeviceInfo) => void;
+  errCallback?: (v: DataStatus) => void;
+  dInfo?: DeviceInfo;
 }
 
 interface DpmContext {
@@ -37,45 +94,69 @@ interface DpmContext {
 }
 
 export class DPM {
-  private reqs: Array<Requests>;
+  private reqs: Array<Request>;
   private started: boolean;
   private con: ACNET;
   private model?: string;
+  private context: Promise<DpmContext>;
 
-  // FIXME: Is the assumption on 'context' valid? Will it always get initialized before it's used?
+  constructor(server?: string, shConn?: ACNET) {
+    let resolveContext!: (val: DpmContext) => void;
 
-  private context!: Promise<DpmContext>;
+    this.reqs = [];
+    this.started = false;
 
-  private async discovery(task: string): Promise<string> {
+    // TODO: The 'shConn' parameter is deprecated; we're going to redo the ACNET module to start a shared worker.
+
+    this.con = shConn ? shConn : new ACNET();
+    this.context = new Promise(resolve => (resolveContext = resolve));
+
+    // This creates a promise which never goes away (because the promise in the .then() call never goes away.)
+
+    Promise.resolve(resolveContext).then(resolve =>
+      this.connectionManager(resolve, server)
+    );
+  }
+
+  // Finds an available DPM for the session. Unlike the public methods, this method is used before we set up the 'context' field. Any changes to this method need to follow that constraint.
+
+  private async discovery(): Promise<string> {
     const msg = new DPM_request_ServiceDiscovery();
 
-    while (true) {
-      const reply = await this.con.oneshot(`${task}@MCAST`, msg, 3000);
+    // We loop until we get a DPM reply.
 
-      if (reply.status.isGood) {
+    while (true) {
+      try {
+        const reply = await this.con.oneshot(`DPMD@MCAST`, msg, 3000);
         const loc: string = await this.con.getName(reply.sender);
 
         console.info(`DPM: Using DPM on ${loc}.`);
-        return loc;
-      } else {
-        console.warn(`DPM: discovery error -- ${reply.status}.`);
+        return `DPMD@${loc}`;
+      } catch (e) {
+        // We received an error. Report the status and sleep for 5 seconds.
+
+        console.warn(`DPM: discovery error -- ${e}.`);
         await new Promise(r => setTimeout(r, 5000));
       }
     }
   }
 
+  // This method returns a function that returns a DPM. If the 'server' parameter specifies a particular DPM, then the returned function simply returns it. If no preference is given, the returned function performs a service discovery.
+
   private findDPM(server?: string): () => Promise<string> {
     if (server !== undefined) {
-      if (server.includes("@")) {
-        const [task, node] = server.split("@");
+      // If 'service' contains an '@', then it's a full, remote task specification. Otherwise it's just a node name and we have to prepend the task name.
 
-        return async () => Promise.resolve(`${task}@${node}`);
-      } else return async () => Promise.resolve(`DPMD@${server}`);
+      const dpm = Promise.resolve(
+        server.includes("@") ? server : `DPMD@${server}`
+      );
+
+      return async () => dpm;
     } else
       return async () => {
-        const node = await this.discovery("DPMD");
+        const node = await this.discovery();
 
-        return Promise.resolve(`DPMD@${node}`);
+        return Promise.resolve(node);
       };
   }
 
@@ -83,29 +164,26 @@ export class DPM {
 
   private static u_reply(reply: Reply<ArrayBuffer>): Reply<DPM_Replies> {
     const { msg, ...copy } = reply;
-    const result: Reply<DPM_Replies> = copy;
 
-    if (msg !== undefined) {
-      const bin = new Uint8Array(msg, msg.byteLength);
+    // DPM returns status in one of the reply messages. If there is no message in the reply packet, then the status in the header indicates the error.
 
-      try {
-        result.msg = DPM_PROTO.unmarshal_reply(bin[Symbol.iterator]());
-      } catch (e) {
-        console.warn(`couldn't decode message -- ignoring`);
-      }
-    }
-    return result;
+    if (msg != undefined) {
+      // Making a copy of the reply message serves several purposes. First, it lets us coerce a Reply<ArrayBuffer> into a Reply<DPM_Replies> in a way that makes Typescript happy. Since Reply<> objects can have a missing 'msg' field and our copy doesn't include the 'msg' field, we're allowed to change its type. The second reason to copy is that the caller gave us a Reply<ArrayBuffer> and it would be rude to change it to a Reply<DPM_replies>.
+
+      const result: Reply<DPM_Replies> = copy;
+      const bin = new Uint8Array(msg, 0, msg.byteLength);
+
+      result.msg = DPM_PROTO.unmarshal_reply(bin[Symbol.iterator]());
+      return result;
+    } else throw new AcnetError(reply.status);
   }
 
   // This is a background task that maintains the ACNET connection.
 
-  private async connectionManager(server?: string) {
-    let resolve!: (val: DpmContext) => void;
-
-    // Reset the context. These will resolve once we reconnect to ACNET and DPM.
-
-    this.context = new Promise(r => (resolve = r));
-
+  private async connectionManager(
+    resolve: (val: DpmContext) => void,
+    server?: string
+  ) {
     const reqOpenList = new DPM_request_OpenList();
     const disc = await this.findDPM(server);
 
@@ -116,29 +194,35 @@ export class DPM {
 
       const replies = await this.con.stream(dpm, reqOpenList, 1000);
 
-      for await (let ii of replies) {
-        const { msg } = DPM.u_reply(ii);
+      try {
+        for await (let ii of replies) {
+          const { msg } = DPM.u_reply(ii);
 
-        if (msg !== undefined) {
           if (msg instanceof DPM_reply_OpenList) {
             console.info(`DPM: using list id ${msg.list_id}`);
 
-            // FIXME: We should resolve the context *after* we set up the list with DPM. There could be pending promises to update the list of requests and we don't want them to occur before DPM is in sync with the current state.
+            // FIXME: We should resolve the context *after* we set up the list with DPM. There could be pending promises to update the list of requests and we don't want them to occur before DPM is in sync with the current state. Unfortunately sendList() and start() require the context to be resolved, so we lose control of restoring DPM's state.
 
             resolve({ listId: msg.list_id, task: dpm });
             await this.sendList();
             if (this.started) await this.start(this.model);
-          } else if (msg instanceof DPM_reply_DeviceInfo)
+          } else if (msg instanceof DPM_reply_DeviceInfo) {
             this.reqs[msg.ref_id].dInfo = msg;
-          else if (msg instanceof DPM_reply_Status) {
+          } else if (msg instanceof DPM_reply_Status) {
+            const { ref_id, timestamp, status } = msg;
             const { errCallback } = this.reqs[msg.ref_id];
 
-            if (errCallback !== undefined) errCallback(msg);
+            if (errCallback !== undefined)
+              errCallback({
+                ref_id,
+                timestamp,
+                status: new Status(status)
+              });
             else {
               const s = new Status(msg.status);
 
               console.info(
-                `DPM: error status ${s} for ${this.reqs[msg.ref_id].request}`
+                `DPM: error status ${s} for ${this.reqs[msg.ref_id].drf2}`
               );
             }
           } else if (
@@ -146,18 +230,30 @@ export class DPM {
             msg instanceof DPM_reply_ScalarArray ||
             msg instanceof DPM_reply_Raw ||
             msg instanceof DPM_reply_Text ||
-            msg instanceof DPM_reply_TextArray ||
+            msg instanceof DPM_reply_TextArray
+          ) {
+            const { ref_id, timestamp, data } = msg;
+            const { callback, dInfo } = this.reqs[ref_id];
+
+            // Forcing Typescript to accept 'dInfo' being defined. The DPM should have sent us the device info before this message, so the summption is valid. If DPM ever breaks this, then lots of Web apps are going to complain.
+
+            callback({ ref_id, timestamp, data }, dInfo as DeviceInfo);
+          } else if (
             msg instanceof DPM_reply_AnalogAlarm ||
             msg instanceof DPM_reply_DigitalAlarm ||
             msg instanceof DPM_reply_BasicStatus
           ) {
-            const { callback, dInfo } = this.reqs[msg.ref_id];
+            const { ref_id, timestamp, cycle, ...rest } = msg;
+            const { callback, dInfo } = this.reqs[ref_id];
 
-            // Forcing Typescript to accept 'tmp.dInfo' being defined. The DPM should have sent us the device info before this message, so the summption is valid. If DPM ever breaks this, then lots of Web apps are going to complain.
+            // Forcing Typescript to accept 'dInfo' being defined. The DPM should have sent us the device info before this message, so the summption is valid. If DPM ever breaks this, then lots of Web apps are going to complain.
 
-            callback(msg, dInfo as DPM_reply_DeviceInfo);
+            callback({ ref_id, timestamp, data: rest }, dInfo as DeviceInfo);
           }
         }
+      } catch (e) {
+        console.error(`DPM: Caught exception -- ${e}`);
+        await replies.cancel();
       }
 
       // Reset our connection parameters so clients block until we re-connect.
@@ -166,19 +262,14 @@ export class DPM {
     }
   }
 
-  constructor(server?: string, shConn?: ACNET) {
-    this.reqs = [];
-    this.started = false;
-    this.con = shConn ? shConn : new ACNET();
-
-    Promise.resolve(server).then(server => this.connectionManager(server));
-  }
-
-  private async sendRequest(ii: number): Promise<any> {
+  private async sendRequest(
+    req: Request,
+    ref_id: number
+  ): Promise<Reply<ArrayBuffer>> {
     const msg = new DPM_request_AddToList();
 
-    msg.ref_id = ii;
-    msg.drf_request = this.reqs[ii].request;
+    msg.ref_id = ref_id;
+    msg.drf_request = req.drf2;
 
     const { listId, task } = await this.context;
 
@@ -188,21 +279,22 @@ export class DPM {
 
   async addRequest(
     req: string,
-    cb: (o: any, i: DPM_reply_DeviceInfo) => boolean,
+    cb: (o: DataReply, i: DeviceInfo) => boolean,
     ecb?: (e: any) => void
   ) {
-    const entry: Requests = { request: req, callback: cb, errCallback: ecb };
+    const entry: Request = { drf2: req, callback: cb, errCallback: ecb };
 
-    // FIXME: There is a race condition. We are registering the request using an index that doesn't exist yet.
+    // FIXME: This is a race condition. We are registering the request using an index that doesn't exist yet in the array.
 
-    const reply = await this.sendRequest(this.reqs.length - 1);
+    const reply = await this.sendRequest(entry, this.reqs.length);
 
     if (reply.status.isGood) this.reqs.push(entry);
     return reply;
   }
 
   private async sendList(): Promise<void> {
-    for (let ii = 0; ii < this.reqs.length; ++ii) await this.sendRequest(ii);
+    for (let ii = 0; ii < this.reqs.length; ++ii)
+      await this.sendRequest(this.reqs[ii], ii);
   }
 
   async start(model?: string): Promise<Reply<DPM_reply_StartList>> {
