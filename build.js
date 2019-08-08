cb1a3b4bd1e3dd617cc4e9e09607bfeafe0fc5f6
Beau Harrison <beau@fnal.gov>
2019-08-08 09:37:41 -0500

    Change to https to fix build.

--- build.js
+++ build.js
@@ -6,14 +6,13 @@ const fs = require('fs');
 const targetDir = 'dist';
 
 if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir);
+  fs.mkdirSync(targetDir);
 }
 
 exec(
-    `curl -Rso DPM.proto 'http://www-ad.fnal.gov/cgi-cvs/cvsweb.cgi/~checkout~/mecca/uls/ul_dpm_protocol/DPM.proto?rev=HEAD;content-type=text%2Fplain' \
+  `curl -Rso DPM.proto 'https://www-ad.fnal.gov/cgi-cvs/cvsweb.cgi/~checkout~/mecca/uls/ul_dpm_protocol/DPM.proto?rev=HEAD;content-type=text%2Fplain' \
 && pc -v -l javascript --js-dts DPM.proto`,
-    error => {
-	if (error)
-	    console.error(error);
-    }
+  error => {
+    if (error) console.error(error);
+  }
 );
